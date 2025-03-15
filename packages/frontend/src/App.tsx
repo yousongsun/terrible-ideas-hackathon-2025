043d7053149f66d0cc7a25c129de7498c2524328
Justin <66831913+stuutzer@users.noreply.github.com>
2025-03-15 20:58:36 +1300

    feat: implemented basic front-end structure

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,33 +1,20 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import siteLogo from '/untitled(3).svg';
+import coverImg from '/cover.webp';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <img src={siteLogo} alt="Website Logo" />
+      </header>
+      <section>
+        <img src={coverImg} alt="Picture of well-to-do man walking his dog." />
+        <p className="cover-subtitle">fig.1: man walking his dog</p>
+      </section>
+      <section>
+        <button className="drumpad">hello</button>
+      </section>
+      <footer>terrible ideas/akl 2025</footer>
     </>
   );
 }
